0ee0f5f9af39400188516eddf337353db82d1bbc
Nick Nekilov <97469315+NickNekilov@users.noreply.github.com>
2022-07-03 00:30:09 +0200

    descript request/response schemas

--- web/src/shared/model.ts
+++ web/src/shared/model.ts
@@ -7,3 +7,51 @@ export type PostInfo = {
   createdAt: string,
   // reactions:
 }
+
+export interface CreateChannelRequest {
+  clientAddress: string;
+  clientPublicKey: string; // In hex.
+}
+
+export interface CreateChannelResponse {
+  channelId: string;
+  clientAddress: string;
+  clientPublicKey: string;
+  clientCurrentBalance: string;
+  clientSeqNo: string;
+  serviceAddress: string;
+  servicePublicKey: string;
+  serviceCurrentBalance: string;
+  serviceSeqNo: string;
+  initialized: boolean;
+}
+
+export interface InitChannelRequest {
+  channelId: string;
+}
+
+export interface InitChannelResponse {
+  success: true;
+}
+
+export interface RequestContentRequest {
+  channelId: string;
+  postCount: number;
+  signature: string;
+  cursor?: string;
+}
+
+export interface RequestContentResponse {
+  posts: PostInfo[];
+  next: string | null;
+}
+
+/**
+ * Note: Endpoint accepts a multipart form with below 2 fields and 1 file "image".
+ */
+export interface CreatePostRequest {
+  title: string;
+  text: string;
+}
+
+export type CreatePostResponse = PostInfo;
