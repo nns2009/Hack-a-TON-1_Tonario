b9125a6035342f02b86582bda399d65ec2723063
Nick Nekilov <97469315+NickNekilov@users.noreply.github.com>
2022-07-02 19:22:32 +0200

    implement S3 upload

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Request } from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
 import { omit } from 'lodash';
 import { keyPairFromSeed } from 'ton-crypto';
@@ -8,14 +8,46 @@ import { PaymentChannel } from '../../web/src/shared/ton/payments/PaymentChannel
 import BN from 'bn.js';
 import { Address, TonClient } from "ton";
 import proxy from 'express-http-proxy';
+import AWS from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { v4 as uuidv4 } from 'uuid';
 
 const config = {
   mongoUrl: process.env.MONGO_URL!,
+
   serviceAddress: process.env.SERVICE_ADDRESS!,
   serviceSeed: process.env.SERVICE_SEED!,
+
   tonUrl: process.env.TON_URL!,
   tonKey: process.env.TON_KEY!,
+
+  s3: {
+    endpoint: process.env.AWS_ENDPOINT!,
+    keyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    bucket: process.env.AWS_S3_BUCKET!,
+  },
 };
+
+interface Channel {
+  clientAddress: string;
+  clientPublicKey: string;
+  clientCurrentBalance: string;
+  clientSeqNo: string;
+  serviceAddress: string;
+  servicePublicKey: string
+  serviceCurrentBalance: string;
+  serviceSeqNo: string;
+  initialized: boolean;
+}
+
+interface Post {
+  title: string;
+  text: string;
+  imageId: string | null;
+  videoId: string | null;
+}
 
 async function run() {
   const app = express();
@@ -26,12 +58,29 @@ async function run() {
     apiKey: config.tonKey,
   });
 
+  const upload = multer({
+    storage: multerS3({
+      s3: new AWS.S3({
+        endpoint: new AWS.Endpoint(config.s3.endpoint),
+        accessKeyId: config.s3.keyId,
+        secretAccessKey: config.s3.secretKey,
+        s3ForcePathStyle: true,
+        signatureVersion: 'v4',
+      }),
+      bucket: config.s3.bucket,
+      key(request: Request, file, callback) {
+        const fileId = uuidv4();
+        callback(null, `images/${fileId}`);
+      },
+    }),
+  });
+
   const serviceKeyPair = keyPairFromSeed(
     Buffer.from(config.serviceSeed, 'hex')
   );
 
-  const channelCollection = mongoClient.db().collection('channels');
-  const postCollection = mongoClient.db().collection('posts');
+  const channelCollection = mongoClient.db().collection<Channel>('channels');
+  const postCollection = mongoClient.db().collection<Post>('posts');
 
   app.use(cors());
   app.use(express.json());
@@ -58,15 +107,15 @@ async function run() {
         clientPublicKey,
       } = req.body;
 
-      const channel = {
+      const channel: Channel = {
         clientAddress,
         clientPublicKey,
-        clientCurrentBalance: 0,
-        clientSeqNo: 0,
+        clientCurrentBalance: new BN(0).toString(10),
+        clientSeqNo: new BN(0).toString(10),
         serviceAddress: config.serviceAddress,
         servicePublicKey: serviceKeyPair.publicKey.toString('hex'),
-        serviceCurrentBalance: 0,
-        serviceSeqNo: 0,
+        serviceCurrentBalance: new BN(0).toString(10),
+        serviceSeqNo: new BN(0).toString(10),
         initialized: false,
       };
 
@@ -123,7 +172,7 @@ async function run() {
         if (channelState !== PaymentChannel.STATE_OPEN) {
           res
             .status(400)
-            .json({ error: 'Channel is not open.'})
+            .json({ error: 'Channel is not open.' })
           return;
         }
 
@@ -149,23 +198,63 @@ async function run() {
     '/request-content',
     async (req, res, next) => {
       const {
+        channelId,
         postCount,
         signature,
       } = req.body;
+
+      const channel = await channelCollection.findOne({
+        _id: new ObjectId(channelId),
+      });
+
+      if (!channel) {
+        return res
+          .status(400)
+          .json({ error: 'Channel does not exist.' });
+      }
+
+      // TODO: Verify signature.
 
       // NOTE: Generate `postCount` posts
       const posts: PostInfo[] = [...new Array(postCount)].map(() => ({
         id: new ObjectId().toString(),
         title: 'Lorem Picsum',
         text: 'Lorem Picsum',
-        imageUrl: 'https://picsum.photos/200',
+        imageUrl: 'https://picsum.photos/800',
         videoUrl: null,
       }));
 
-      // TODO: Bill for `posts.length` posts.
-
       try {
         res.json(posts);
+      } catch (error) {
+        next(error);
+      }
+    },
+  );
+
+  app.post(
+    '/create-post',
+    upload.single('image'),
+    async (req, res, next) => {
+      const { title, text } = req.body;
+
+      try {
+        const { key: imageKey } = req.file as any;
+        const imageId = imageKey.replace(/^images\//, '');
+
+        const post: Post = {
+          title,
+          text,
+          imageId,
+          videoId: null,
+        };
+
+        const { insertedId: postId } = await postCollection.insertOne(post);
+
+        res.json({
+          id: postId,
+          ...omit(post, '_id'),
+        });
       } catch (error) {
         next(error);
       }
