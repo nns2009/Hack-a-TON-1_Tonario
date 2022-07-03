92245038cd0452d1a97bafa6765ad8b430f35cac
Igor Konyakhin <nns2009@yandex.ru>
2022-07-03 11:40:18 +0300

    Infinite Scroll works

--- web/src/shared/model.ts
+++ web/src/shared/model.ts
@@ -64,7 +64,7 @@ export interface ReactRequest {
   channelId: string;
   signature: string;
   postId: string;
-  reactionType: string;
+  reactionType: 'like' | 'fire' | 'brilliant';
 }
 
 export interface ReactResponse {
