b16d7affbd48ed62f3931f2b7d2835a78ba3c55a
Andrew <43111428+pyAndr3w@users.noreply.github.com>
2022-07-03 06:59:36 +0500

    Create PRICES.ts

---
+++ web/src/shared/PRICES.ts
@@ -0,0 +1,13 @@
+import {toNano} from "ton";
+
+const VIEW = toNano(0.0001);
+const LIKE = toNano(0.01);
+const FIRE = toNano(0.1);
+const DIAMOND = toNano(1);
+
+export default {
+    VIEW,
+    LIKE,
+    FIRE,
+    DIAMOND
+}
